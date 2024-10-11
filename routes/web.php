7575fb103dec6a4fe39612846f050f5bdc9b8373
brendavrossetto <104913779+brendavrossetto@users.noreply.github.com>
2024-10-11 11:19:15 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return response() -> json ([
         'message' => 'ok'
     ]);
+});
+
+Route::get('/somar', function(Request $request) {
+    $soma= array_sum($request->all());
+    return response()->json([
+        'message' => 'somado com sucesso',
+        'sum' => $soma,
+    ]);
+
 });
