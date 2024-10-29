75508de26b1e70f665e1f0ef3bde5b06bdfb0f97
brendavrossetto <104913779+brendavrossetto@users.noreply.github.com>
2024-10-29 08:47:17 -0300

    desativar csfr

--- app/Http/Requests/StorePeopleRequest.php
+++ app/Http/Requests/StorePeopleRequest.php
@@ -12,7 +12,7 @@ class StorePeopleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -7,36 +7,33 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return response() -> json ([
+    return response()->json([
         'message' => 'ok'
     ]);
 });
 
-Route::get('/somar', function(Request $request) {
-    if (count($request->all())< 1) {
+Route::get('/somar', function (Request $request) {
+    if (count($request->all()) < 1) {
         return response()->json([
-            'massage'=> 'Não há valores para somar'
+            'massage' => 'Não há valores para somar'
         ], 406);
     }
-    $soma= array_sum($request->all());
+    $soma = array_sum($request->all());
     return response()->json([
         'message' => 'somado com sucesso',
         'sum' => $soma,
     ]);
-
 });
 
 // /people/list 
-Route::prefix('/people')->group(function() {
-    Route::get('/list', 
-    [PeopleController::class, 'list']);
+Route::prefix('/people')->group(function () {
+    Route::get(
+        '/list',
+        [PeopleController::class, 'list']
+    );
 
-Route::post('/store',
-[PeopleController::class, 'store']);
-
+    Route::post(
+        '/store',
+        [PeopleController::class, 'store']
+    );
 });
-
-
-
-
-
